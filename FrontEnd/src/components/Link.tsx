72ab7829fb089d09c3e2c199d37936a58e2ebf45
Cristian Frunza <cristian.frunza@isa.utm.md>
2026-02-09 00:09:09 +0200

    links for different pages

---
+++ FrontEnd/src/components/Link.tsx
@@ -0,0 +1,26 @@
+﻿import {Anchor} from "@mui/icons-material";
+type Props = {
+    page: string;
+    selectedPage: string;
+    setSelectedPage: (value: string) => void;
+}
+const Link   = ({
+    page,
+    selectedPage, 
+    setSelectedPage,
+     }: Props) => {
+    const lowerCasePage = page.toLowerCase().replace(/ /g, "");
+        
+    return (
+        <Anchor
+            className={`${selectedPage === lowerCasePage ? "text-primary-500" : ""} 
+            transition duration-500 hover:text-primary-300
+            `}
+        href={'#${lowerCasePage}'}
+        onClick={() => setSelectedPage(lowerCasePage)}
+        >
+            {page}
+        </Anchor>
+    )
+}
+export default Link;
