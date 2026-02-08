3116e631d7c6387af10203f4a275f67e1e7c6116
Cristian Frunza <cristian.frunza@isa.utm.md>
2026-02-08 21:54:04 +0200

    debug logo

--- FrontEnd/src/components/Index.tsx
+++ FrontEnd/src/components/Index.tsx
@@ -7,10 +7,10 @@ const NavBar = () => {
         <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
             <div className={`${flexBetween} mx-auto w-5/6`}>
                 <div className={`${flexBetween} w-full gap-16`}>
-                    /* Left side */
-                    <img src={Logo} alt="logo" className='w-16'/>
-                    
-                    /* Right side */
+                    { /* Left side */}
+                    <img src={Logo} alt="logo" className='w-30'/>
+
+                    {/* Right side */} 
                     <div className={`${flexBetween} w-full`}>
                         <div className={`${flexBetween} gap-8 text-sm`}>
                             <p>Acasă</p>
