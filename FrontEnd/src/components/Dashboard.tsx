733ab03ce328579f325d33e5d6149b247e2ca10d
Cristian Frunza <cristian.frunza@isa.utm.md>
2026-02-08 18:29:17 +0200

    new log-in logic

---
+++ FrontEnd/src/components/Dashboard.tsx
@@ -0,0 +1,17 @@
+﻿const Dashboard = () => {
+    return (
+        <div>
+            <h1>Dashboard</h1>
+            <button
+                onClick={() => {
+                    localStorage.removeItem("auth");
+                    window.location.reload();
+                }}
+            >
+                Logout
+            </button>
+        </div>
+    );
+};
+
+export default Dashboard;
