682acac72ac413d3d95017d016af2be8fa8f44bd
Bîrcă Damian <damibirca@gmail.com>
2026-02-11 00:35:53 +0200

    Add Schedule page with route

--- FrontEnd/src/routes/AppRoutes.tsx
+++ FrontEnd/src/routes/AppRoutes.tsx
@@ -1,12 +1,14 @@
 ﻿import { Routes,Route } from "react-router-dom";
 import Dashboard from "../pages/Dashboard";
 import Students from "../pages/Students";
+import Orar from "../pages/Orar";
 
 function AppRoutes() {
     return (
         <Routes>
             <Route path="/" element={<Dashboard />} />
             <Route path="/students" element={<Students />} />
+            <Route path="/orar" element={<Orar />} />
         </Routes>
     );
 }
