3eb9dd821c11f059b1ac21399a0d0514a18217b8
Bîrcă Damian <damibirca@gmail.com>
2026-02-11 00:35:53 +0200

    Add Schedule page with route

---
+++ FrontEnd/src/pages/Orar.tsx
@@ -0,0 +1,10 @@
+﻿// src/pages/Schedule.tsx
+const Orar = () => {
+    return (
+        <div className="p-8">
+            <h1 className="text-2xl font-bold mb-4">Orar Școlar</h1>
+        </div>
+    );
+};
+
+export default Orar;
