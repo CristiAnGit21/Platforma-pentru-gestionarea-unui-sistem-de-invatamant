0eda72593cfbfe5e291656712690f6660cff77de
Cristian Frunza <cristian.frunza@isa.utm.md>
2026-02-10 20:44:04 +0200

    debug+notifications file

--- FrontEnd/src/components/Schedule.tsx
+++ FrontEnd/src/components/Schedule.tsx
@@ -1,20 +1,22 @@
-﻿const TodaySchedule = () => {
-    <div className="bg-white rounded-x1 p-4 shadow-sm">
-        <h2 className="text-xl font-semibold mb-4">Orar pentru astăzi</h2>
-       <ul className="space-y-3">
-              <li className="flex justify-between">
-            <span> 08:00 Programrea orientata pe obiect</span>
-            <span className="text-gray-500">Sala 101</span>
-        </li>
-        <li className="flex justify-between bg-violet-50 p-2 rounded">
-            <span> 10:00 Baze de date</span>
-            <span className="text-gray-500">Sala 202</span>
-        </li>
-        <li className="flex justify-between">
-            <span> 12:00 Sisteme de operare</span>
-            <span className="text-gray-500">Sala 303</span>
-        </li>
-       </ul>
-    </div>
+﻿const Schedule = () => {
+    return (
+        <div className="bg-white rounded-xl p-4 shadow-sm">
+            <h2 className="text-xl font-semibold mb-4">Orar pentru astăzi</h2>
+           <ul className="space-y-3">
+                  <li className="flex justify-between">
+                <span> 08:00 Programrea orientata pe obiect</span>
+                <span className="text-gray-500">Sala 101</span>
+            </li>
+            <li className="flex justify-between bg-violet-50 p-2 rounded">
+                <span> 10:00 Baze de date</span>
+                <span className="text-gray-500">Sala 202</span>
+            </li>
+            <li className="flex justify-between">
+                <span> 12:00 Sisteme de operare</span>
+                <span className="text-gray-500">Sala 303</span>
+            </li>
+           </ul>
+        </div>
+    );
 }
-export default TodaySchedule;
+export default Schedule;
