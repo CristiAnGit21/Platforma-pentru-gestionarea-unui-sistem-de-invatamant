107dae894763247e430d2eb5a6174d27580dcd89
Bîrcă Damian <damibirca@gmail.com>
2026-02-11 00:39:44 +0200

    Make navbar to be fixed in pages

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,12 +1,25 @@
+import { useState } from 'react';
 import LogIn from "./pages/LogIn";
-import { BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
+import NavBar from "./components/NavBar";
 
 function App() {
+    const [selectedPage, setSelectedPage] = useState("Acasă");
     const isAuth = localStorage.getItem("auth") === "true";
+
     return (
         <BrowserRouter>
-            {isAuth ? <AppRoutes /> : <LogIn />}
+            {isAuth ? (
+                <div className="flex">
+                    <NavBar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+                    <main className="flex-1 ml-24 min-h-screen bg-gray-50">
+                        <AppRoutes />
+                    </main>
+                </div>
+            ) : (
+                <LogIn />
+            )}
         </BrowserRouter>
     );
 }
