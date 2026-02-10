3dcb6821e735e4557a65d28e274c9acabfaa35f7
Cristian Frunza <cristian.frunza@isa.utm.md>
2026-02-11 00:54:47 +0200

    update hours

--- FrontEnd/src/components/Schedule.tsx
+++ FrontEnd/src/components/Schedule.tsx
@@ -8,11 +8,11 @@
                 <span className="text-gray-500">Sala 101</span>
             </li>
             <li className="flex justify-between bg-violet-50 p-2 rounded">
-                <span> 10:00 Baze de date</span>
+                <span> 09:30 Baze de date</span>
                 <span className="text-gray-500">Sala 202</span>
             </li>
             <li className="flex justify-between">
-                <span> 12:00 Sisteme de operare</span>
+                <span> 11:00 Sisteme de operare</span>
                 <span className="text-gray-500">Sala 303</span>
             </li>
            </ul>
