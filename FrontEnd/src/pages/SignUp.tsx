d94b9b3d871639ba5c9e1d6892afda12597f772c
Bîrcă Damian <damibirca@gmail.com>
2026-02-11 19:46:01 +0200

    add signup page and route to it

---
+++ FrontEnd/src/pages/SignUp.tsx
@@ -0,0 +1,12 @@
+﻿const SignUp = () => {
+    return (
+        <div>
+            <h1 className="text-4xl font-bold text-gray-800">Pagina de SignUp</h1>
+        </div>
+    );
+};
+
+export default SignUp;
+
+
+
