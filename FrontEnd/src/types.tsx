4388f072fb8ab8f58915d5270a554de5b2ff4e29
Cristian Frunza <cristian.frunza@isa.utm.md>
2026-02-19 00:26:01 +0200

    add types for Grade, Student, and GroupData

---
+++ FrontEnd/src/types.tsx
@@ -0,0 +1,20 @@
+﻿
+export type Grade = {
+    id: string;
+    value: number;
+    date: string;
+};
+
+export type Student = {
+    id: string;
+    firstName: string;
+    lastName: string;
+    group: string;
+    grades: Grade[];
+    attendance: number; // percentage (0-100)
+};
+
+export type GroupData = {
+    name: string;
+    students: Student[];
+};
