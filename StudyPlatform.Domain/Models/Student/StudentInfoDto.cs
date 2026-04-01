e9079bea90dac197b2685a8fba289d2b64de91ae
Gabriel Bulimestru <gabriel.bulimestru@isa.utm.md>
2026-04-01 10:58:35 +0300

    crearea subfolderului student in folderul models si adaugare studentcreatedto si studentinfodto

---
+++ StudyPlatform.Domain/Models/Student/StudentInfoDto.cs
@@ -0,0 +1,8 @@
+﻿namespace StudyPlatform.Domain.Models.Student;
+
+public class StudentInfoDto
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
