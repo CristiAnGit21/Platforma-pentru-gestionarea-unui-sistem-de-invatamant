6bd681958eca787c16f82a3668e870009e03e9ef
Gabriel Bulimestru <gabriel.bulimestru@isa.utm.md>
2026-04-01 10:50:16 +0300

    adaugarea de businesslogin in businesslayer

---
+++ StudyPlatform.BusinessLayer/BusinessLogic.cs
@@ -0,0 +1,14 @@
+﻿using StudyPlatform.BusinessLayer.Core;
+using StudyPlatform.BusinessLayer.Interfaces;
+
+namespace StudyPlatform.BusinessLayer;
+
+public class BusinessLogic
+{
+    public BusinessLogic() { }
+
+    public IStudentLogic GetStudentLogic()
+    {
+        return new StudentLogic();
+    }
+}
