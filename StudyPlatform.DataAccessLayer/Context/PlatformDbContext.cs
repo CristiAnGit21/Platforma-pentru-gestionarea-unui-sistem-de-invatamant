e3c31c7140bc153ff114e301905adb5954b6f198
Gabriel Bulimestru <gabriel.bulimestru@isa.utm.md>
2026-04-01 11:00:17 +0300

    crearea folderului context in dataaccesslayer si adaugarea platformdbcontext

---
+++ StudyPlatform.DataAccessLayer/Context/PlatformDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudyPlatform.Domain.Entities.Student;
+
+namespace StudyPlatform.DataAccessLayer.Context;
+
+public class PlatformDbContext: DbContext
+{
+    public DbSet<StudentEntity> Students { get; set; }
+    // public DbSet<CategoryEntity> Categories { get; set; }
+    // public DbSet<StudentCategoryEntity> StudentCategories { get; set; }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=studyplatform;Username=postgres;Password=postgres;");
+        }
+    }
+    
+}
