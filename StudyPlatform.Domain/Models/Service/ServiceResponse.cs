8e81d42e6c79b1417aad2813965c03cca2ebcde0
Gabriel Bulimestru <gabriel.bulimestru@isa.utm.md>
2026-04-01 10:57:19 +0300

    crearea subfolderului services si adaugarea serviceresponse

---
+++ StudyPlatform.Domain/Models/Service/ServiceResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace StudyPlatform.Domain.Models.Service;
+
+public class ServiceResponse
+{
+    public bool IsSuccess { get; set; }
+    public string? Message { get; set; }
+    public object? Data { get; set; }
+    
+    //HttpStatusCode
+}
